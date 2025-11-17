8898afcace4d040ba6aaa6fae9735a054d65a74c
Alexander Koglin <alexander.koglin@uni-greifswald.de>
2025-11-17 11:33:28 +0100

    added base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 export default defineNuxtConfig({
   ssr: false,
   modules: ['@nuxt/content'],
+  app: {
+    baseURL: '/lyrics/'
+  },
+  $development: {
+    app: {
+      baseURL: '/',
+    }
+  },
   nitro: {
     prerender: {
       routes: ['/']
